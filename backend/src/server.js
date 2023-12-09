f47bd7c91d8bed042bfdad892029b3258f40b7aa
zeu22 <150097376+zeu22@users.noreply.github.com>
2023-12-08 21:40:35 -0400

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-app.set('port', process.env.PORT || 4000);
-
-// ================ MIDDLEWARES ================
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-
+const express = require('express'); 
+const cors = require('cors'); 
+ 
+const app = express(); 
+ 
+app.set('port', process.env.PORT || 4000); 
+ 
+// ================ MIDDLEWARES ================ 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+app.use(cors()); 
+ 
+// ================= ROUTES ==================== 
+const contactanosRoute = require('./routes/contactanos'); 
+ 
+// ================= ENDPOINTS ==================== 
+app.use('/contactanos', contactanosRoute); 
+ 
 module.exports = app;
